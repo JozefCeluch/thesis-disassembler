112c326a97745b3a649c83be2c3ff354e2f95a1f
Jozef Celuch <celuchd@gmail.com>
2014-08-23 17:19:46 +0200

    basic decompiler visitor classes

---
+++ src/com/thesis/file/DecompilerClassVisitor.java
@@ -0,0 +1,78 @@
+package com.thesis.file;
+
+import org.objectweb.asm.*;
+import org.objectweb.asm.util.Printer;
+import org.objectweb.asm.util.Textifier;
+
+public class DecompilerClassVisitor extends ClassVisitor {
+
+    private final Printer printer;
+
+    public DecompilerClassVisitor() {
+        this(new Textifier()); //todo create own printer
+    }
+
+    public DecompilerClassVisitor(Printer printer) {
+        super(Opcodes.ASM5);
+        this.printer = printer;
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        printer.visit(version, access, name, signature, superName, interfaces);
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public void visitSource(String source, String debug) {
+        printer.visitSource(source, debug);
+        super.visitSource(source, debug);
+    }
+
+    @Override
+    public void visitOuterClass(String owner, String name, String desc) {
+        printer.visitOuterClass(owner, name, desc);
+        super.visitOuterClass(owner, name, desc);
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+        Printer p = printer.visitClassAnnotation(desc, visible);
+        AnnotationVisitor av = cv == null ? null : cv.visitAnnotation(desc, visible);
+        return new DecompilerAnnotationVisitor(av, p);
+    }
+
+    @Override
+    public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String desc, boolean visible) {
+        return super.visitTypeAnnotation(typeRef, typePath, desc, visible);
+    }
+
+    @Override
+    public void visitAttribute(Attribute attr) {
+        printer.visitClassAttribute(attr);
+        super.visitAttribute(attr);
+    }
+
+    @Override
+    public void visitInnerClass(String name, String outerName, String innerName, int access) {
+        printer.visitInnerClass(name, outerName, innerName, access);
+        super.visitInnerClass(name, outerName, innerName, access);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+        return super.visitField(access, name, desc, signature, value);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        cv.visitMethod(access, name, desc, signature, exceptions);
+        return super.visitMethod(access, name, desc, signature, exceptions);
+    }
+
+    @Override
+    public void visitEnd() {
+        printer.visitClassEnd();
+        super.visitEnd();
+    }
+}
