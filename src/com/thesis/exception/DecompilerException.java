935c5b1827be7bd7699903930df1290425138bba
Jozef Celuch <celuchd@gmail.com>
2015-04-18 13:58:14 +0200

    created decompiler exception, not used yet

---
+++ src/com/thesis/exception/DecompilerException.java
@@ -0,0 +1,12 @@
+package com.thesis.exception;
+
+public class DecompilerException extends Exception {
+
+	public DecompilerException(String message) {
+		super(message);
+	}
+
+	public DecompilerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
